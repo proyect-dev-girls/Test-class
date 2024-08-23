49d7e9d878f79c127969cc81e9b53889badd8eb3
milagrosVillalba <soledad110203@gmail.com>
2024-08-23 14:30:27 -0300

    mostrar quiz

--- Frond/quizclass/src/components/DashboardAdmin/sindebar/alumnos/Alumnos.jsx
+++ Frond/quizclass/src/components/DashboardAdmin/sindebar/alumnos/Alumnos.jsx
@@ -1,17 +1,20 @@
-// src/components/DashboardAdmin/sindebar/alumnos/Alumnos.jsx
-import React, { useContext, useState, useEffect } from 'react';
-import { QuizContext } from '../../../../contexts/quizContexts';
-import imageNene from '../../../../assets/image/nene.png';
+import React, { useState, useEffect } from 'react';
 import { motion, useAnimation } from 'framer-motion';
 import { Link } from 'react-router-dom';
 
 const Alumnos = () => {
-  const { quizzes } = useContext(QuizContext);
+  const [quizzes, setQuizzes] = useState([]);
   const [selectedQuiz, setSelectedQuiz] = useState(null);
+  const [selectedAnswer, setSelectedAnswer] = useState('');
+  const [feedback, setFeedback] = useState('');
   const sidebarControls = useAnimation();
   const [bgColor, setBgColor] = useState('#f48ca4'); // Color inicial
 
   useEffect(() => {
+    // Leer los quizzes del localStorage
+    const storedQuizzes = JSON.parse(localStorage.getItem('quizzes')) || [];
+    setQuizzes(storedQuizzes);
+
     // Animación de cambio de color de fondo en la barra lateral
     const intervalId = setInterval(() => {
       setBgColor(prevColor => (prevColor === '#f48ca4' ? '#a4cc64' : '#f48ca4'));
@@ -27,11 +30,28 @@ const Alumnos = () => {
 
   const handleStartQuiz = (quiz) => {
     setSelectedQuiz(quiz);
+    setSelectedAnswer('');
+    setFeedback('');
+  };
+
+  const handleAnswerChange = (event) => {
+    setSelectedAnswer(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    if (!selectedQuiz) return;
+
+    // Verifica si la respuesta seleccionada es correcta
+    const isCorrect = selectedAnswer === selectedQuiz.correctAnswer;
+    if (isCorrect) {
+      setFeedback('¡Respuesta Correcta!');
+    } else {
+      setFeedback('Respuesta Incorrecta. Inténtalo de nuevo.');
+    }
   };
 
   return (
     <div className="flex h-screen bg-gray-100">
-      {/* Barra lateral */}
       <motion.div
         className="w-64 text-white p-4 flex flex-col space-y-4"
         animate={sidebarControls}
@@ -51,60 +71,72 @@ const Alumnos = () => {
             <span className="text-xl mr-2">⚙️</span>
             <span>Configuración</span>
           </button>
-          {/* cerrar sesion*/}
           <Link to={'/'} className="flex items-center p-2 rounded-lg hover:bg-[#f48ca4] transition duration-300">
             <span className="text-xl mr-2">🔒</span>
             <span>Salir</span>
           </Link>
-
         </div>
       </motion.div>
 
-      {/* Área de contenido */}
-      <div className="flex-1 p-6 relative">
-        <div className="relative bg-white rounded-lg shadow-lg p-6">
-          {/* Nube con el texto */}
-          <div className="absolute top-4 left-1/2 transform -translate-x-1/2 bg-[#a4cc64] p-4 rounded-lg shadow-lg border border-gray-300 text-center">
-            <p className="text-lg font-semibold">Bienvenido, Alumno</p>
-          </div>
-
-          {/* Imagen del niño */}
-          <motion.img
-            src={imageNene}
-            alt="Nene"
-            className="absolute top-12 left-1/3 transform -translate-x-1/2 w-48 h-48"
-            animate={{ y: [0, -10, 0] }}
-            transition={{ duration: 1, ease: 'easeInOut', repeat: Infinity }}
-          />
-          
-          <h1 className="text-3xl font-bold mb-6 text-[#f48ca4] ">Quizzes Disponibles 🎓</h1>
-          {quizzes.length === 0 ? (
-            <p className="text-gray-600 s">No hay quizzes disponibles.</p>
-          ) : (
-            quizzes.map((quiz, index) => (
-              <div key={index} className="mb-4 bg-white p-4 rounded-lg shadow-md border border-[#a4cc64]">
-                <h2 className="text-2xl font-bold text-[#f48ca4] mb-4">{`Quiz ${index + 1}`}</h2>
-                <p className="mb-2"><strong>Pregunta:</strong> {quiz.question}</p>
-                <p className="mb-2"><strong>Opciones:</strong> {quiz.options.join(', ')}</p>
-                <p className="mb-2"><strong>Respuesta Correcta:</strong> {quiz.answer}</p>
-                {quiz.image && <img src={quiz.image} alt="Quiz" className="w-20 h-20 object-cover rounded-md border border-[#a4cc64]" />}
-                <button
-                  onClick={() => handleStartQuiz(quiz)}
-                  className="mt-4 bg-[#a4cc64] text-white px-4 py-2 rounded-lg hover:bg-[#8bbd4e] transition duration-300"
-                >
-                  Comenzar Quiz
-                </button>
-              </div>
-            ))
-          )}
-
-          {selectedQuiz && (
-            <div className="mt-6 p-6 bg-[#f48ca4] rounded-lg text-white">
-              <h2 className="text-2xl font-bold mb-4">Estás jugando el Quiz {selectedQuiz.question}</h2>
-              {/* Aquí puedes agregar la lógica para jugar el quiz */}
+      <div className="flex-1 p-8 bg-gradient-to-r from-[#f48ca4] to-[#a4cc64]">
+        <h1 className="text-3xl font-bold text-center mb-6 text-[#ffffff]">Quizzes Disponibles</h1>
+        
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+          {quizzes.map((quiz, index) => (
+            <div
+              key={index}
+              className="bg-white p-6 rounded-lg shadow-lg border border-[#a4cc64] transition-transform transform hover:scale-105 cursor-pointer"
+              onClick={() => handleStartQuiz(quiz)}
+            >
+              <h2 className="text-xl font-bold mb-2 text-[#a4cc64]">Pregunta {index + 1}</h2>
+              {quiz.image && (
+                <img
+                  src={quiz.image}
+                  alt="Imagen de la pregunta"
+                  className="w-40 h-32 object-cover rounded-md border border-[#a4cc64] shadow-lg mb-4"
+                />
+              )}
+              <p className="text-gray-700 mb-4">{quiz.question}</p>
             </div>
-          )}
+          ))}
         </div>
+
+        {selectedQuiz && (
+          <div className="mt-8 p-6 bg-white rounded-lg shadow-lg border border-[#a4cc64]">
+            <h2 className="text-2xl font-bold mb-4 text-[#a4cc64]">Quiz Seleccionado</h2>
+            {selectedQuiz.image && (
+              <img
+                src={selectedQuiz.image}
+                alt="Imagen del quiz"
+                className="w-40 h-32 object-cover rounded-md border border-[#a4cc64] shadow-lg mb-4"
+              />
+            )}
+            <p className="text-gray-700 mb-4">{selectedQuiz.question}</p>
+            {selectedQuiz.options.map((option, index) => (
+              <div key={index} className="mb-2">
+                <input
+                  type="radio"
+                  id={`option-${index}`}
+                  name="quiz-option"
+                  value={option}
+                  onChange={handleAnswerChange}
+                />
+                <label htmlFor={`option-${index}`} className="ml-2 text-gray-700">{option}</label>
+              </div>
+            ))}
+            <button
+              onClick={handleSubmit}
+              className="bg-[#a4cc64] text-white px-4 py-2 rounded-lg hover:bg-[#8bbd4e] transition-colors duration-300 ease-in-out"
+            >
+              Enviar Respuesta
+            </button>
+            {feedback && (
+              <p className={`mt-4 text-lg ${feedback.includes('Correcta') ? 'text-green-500' : 'text-red-500'}`}>
+                {feedback}
+              </p>
+            )}
+          </div>
+        )}
       </div>
     </div>
   );
